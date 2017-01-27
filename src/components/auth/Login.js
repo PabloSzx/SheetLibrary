f6dfd7e74f2ce4992d9293ab2f60313ff7b6bd46
Pablo Sáez Parra <pablosaez1995@gmail.com>
2017-01-27 17:11:02 -0300

    v1.2
    login with facebook,google,twitter, and pendind github

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import * as firebase from 'firebase';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router';
+import facebookButton from '../../img/facebookButton.png';
+import googleButton from '../../img/googleButton.png';
+import twitterButton from '../../img/twitterButton.png';
+import githubButton from '../../img/githubButton.png';
 
 class Login extends React.Component {
 	state = {
@@ -24,12 +28,71 @@ class Login extends React.Component {
 		this.setState(change);
 	}
 
+	fblogin() {
+		firebase.auth().signOut();
+		var provider = new firebase.auth.FacebookAuthProvider();
+
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // var token = result.credential.accessToken;
+			// var user = result.user;
+		  // this.setState({ 'asd' : user });
+		}).catch(function(error) {
+		  // var errorCode = error.code;
+		  // var errorMessage = error.message;
+		  // var email = error.email;
+		  // var credential = error.credential;
+		});
+
+		//firebase.auth().signInWithRedirect(provider);
+	}
+
+	googlelogin() {
+		var provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+			// var token = result.credential.accessToken;
+			// var user = result.user;
+		}).catch(function(error) {
+			// var errorCode = error.code;
+			// var errorMessage = error.message;
+			// var email = error.email;
+			// var credential = error.credential;
+		});
+	}
+
+	twitterlogin() {
+		var provider = new firebase.auth.TwitterAuthProvider();
+
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // var token = result.credential.accessToken;
+		  // var secret = result.credential.secret;
+		  // var user = result.user;
+		}).catch(function(error) {
+		  // var errorCode = error.code;
+		  // var errorMessage = error.message;
+		  // var email = error.email;
+		  // var credential = error.credential;
+});
+	}
+
+	githublogin() {
+		var provider = new firebase.auth.GithubAuthProvider();
+
+		firebase.auth().signInWithRedirect(provider).then(function(result) {
+		  // var token = result.credential.accessToken;
+		  // var user = result.user;
+			}).catch(function(error) {
+			  // var errorCode = error.code;
+			  // var errorMessage = error.message;
+			  // var email = error.email;
+			  // var credential = error.credential;
+		});
+	}
 	render() {
 		var errors = this.state.error ? <p> {this.state.error} </p> : '';
 		return (
 			<div>
-				<h1>Login</h1>
-				<form onSubmit={this.handleSubmit.bind(this)}>
+				<h1><strong>Login</strong></h1>
+				<form className="hidden" onSubmit={this.handleSubmit.bind(this)}>
 					<label>Email <input type='email'
 					                    placeholder='Email'
 					                    value={this.state.email}
@@ -44,8 +107,20 @@ class Login extends React.Component {
 
 					{errors}
 					<br/>
-					<button className='btn btn-primary' type='submit'>Login</button>
+					<button className='btn btn-primary' type='submit'>Email Login</button>
 				</form>
+				<img src={facebookButton} alt="Sign in with Facebook" className='loginButton btn' onClick={()=> this.fblogin()}/>
+				<br/>
+				<img src={googleButton} alt="Sign in with Google" className='loginButton btn' onClick={()=> this.googlelogin()}/>
+				<br/>
+				<img src={twitterButton} alt="Sign in with Twitter" className='loginButton btn' onClick={()=> this.twitterlogin()}/>
+				<br/>
+				<img src={githubButton} alt="Sign in with GitHub" className='loginButton btn' onClick={()=> this.githublogin()}/>
+
+				{/* <button className='btn btn-primary' onClick={()=> this.fblogin()}>Facebook Login</button> */}
+				<br/>
+				<br/>
+				<Link to='/'><div className="btn btn-warning">Cancel</div></Link>
 			</div>
 		);
 	}
